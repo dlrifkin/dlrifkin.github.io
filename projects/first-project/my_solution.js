ec9ada5bb428a01dc11d30af33c3bfb4bf93b76d
dlrifkin <davidlrifkin@gmail.com>
2014-11-30 13:29:10 -0500

    all

--- projects/first-project/my_solution.js
+++ projects/first-project/my_solution.js
@@ -64,7 +64,7 @@ function run(){
 		runPic.style.visibility = 'visible';
 		brainPic.style.visibility = 'visible';
 		pinkyPic.style.visibility = 'hidden';
-		setTimeout(function(){alert("Now help Braineat and drink.")}, 700);
+		setTimeout(function(){alert("Now help Brain eat and drink.")}, 100);
 
  	ran = true;
   	}
